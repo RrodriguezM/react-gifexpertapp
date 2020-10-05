1df547a6fee4a32fa95856b900a7991053989aa3
Rafael Rodriguez <rafael.a.rodriguez.m@gmail.com>
2020-10-05 10:11:43 -0500

    docs added

---
+++ docs/precache-manifest.f8b928b905ecea1f99fc26b3761cdc90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7424c7465a3c6cf0cce0bc9dae04bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fef91b99fa6e85465475",
+    "url": "/static/css/main.179beedc.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef91b99fa6e85465475",
+    "url": "/static/js/main.c47668ac.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
