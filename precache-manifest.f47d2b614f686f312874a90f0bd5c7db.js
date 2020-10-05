66221cb584ce88adb6150e90984d19b2be639914
Rafael Rodriguez <rafael.a.rodriguez.m@gmail.com>
2020-10-05 10:56:40 -0500

    Updates

---
+++ precache-manifest.f47d2b614f686f312874a90f0bd5c7db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a7a70bfb9ee76f7fce1f944f483dd55",
+    "url": "/react-gifexpertapp/index.html"
+  },
+  {
+    "revision": "fef91b99fa6e85465475",
+    "url": "/react-gifexpertapp/static/css/main.179beedc.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/react-gifexpertapp/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gifexpertapp/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef91b99fa6e85465475",
+    "url": "/react-gifexpertapp/static/js/main.c47668ac.chunk.js"
+  },
+  {
+    "revision": "8d60da8017703a69006d",
+    "url": "/react-gifexpertapp/static/js/runtime-main.ae19a04c.js"
+  }
+]);
